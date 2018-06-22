5e0f43b68e9df077bae50a97f150b3e0282aadb8
bartosz <abraxaswisz@gmail.com>
2018-06-22 13:35:31 +0200

    created basic add method

--- src/Components/AddCDForm.js
+++ src/Components/AddCDForm.js
@@ -1,18 +1,32 @@
 import React, { Component, Fragment } from "react";
 
 class AddCDForm extends Component {
+  artistRef = React.createRef();
+  albumRef = React.createRef();
+  yearRef = React.createRef();
+
+  createCD = e => {
+    e.preventDefault();
+    console.log(this.artistRef.current.value);
+    const cd = {
+      artist: this.artistRef.current.value,
+      album: this.artistRef.current.value,
+      year: this.yearRef.current.value
+    };
+    this.props.addCD(cd);
+  };
   render() {
     return (
       <Fragment>
-        <form>
+        <form onSubmit={this.createCD}>
           <label htmlFor="artist">Artist</label>
-          <input id="artist" type="text" />
+          <input ref={this.artistRef} id="artist" type="text" />
           <br />
           <label htmlFor="album">Album</label>
-          <input id="album" type="text" />
+          <input ref={this.albumRef} id="album" type="text" />
           <br />
           <label htmlFor="year">Year</label>
-          <input id="year" type="text" />
+          <input ref={this.yearRef} id="year" type="text" />
           <br />
           <button type="submit">Submit</button>
         </form>
--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
 import "../App.css";
 import AddCDForm from "./AddCDForm";
+import CDCollection from "./CDCollection";
 
 class App extends Component {
+  state = {
+    cds: {}
+  };
+  addCD = cd => {
+    const cds = { ...this.state.cds };
+    cds[`cd${Date.now()}`] = cd;
+    this.setState({
+      cds
+    });
+  };
+  updateCD = () => {};
+  removeCD = () => {};
   render() {
     return (
       <React.Fragment>
-        <AddCDForm />
+        <AddCDForm
+          addCD={this.addCD}
+          updateCD={this.updateCD}
+          removeCD={this.removeCD}
+        />
+        <CDCollection />
       </React.Fragment>
     );
   }
