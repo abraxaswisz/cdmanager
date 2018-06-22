d7d74b256dff3bdd718d48754fc8228cc8cfeb5c
bartosz <abraxaswisz@gmail.com>
2018-06-22 11:20:10 +0200

    added and injected simple AddCDForm

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import "../App.css";
+import AddCDForm from "./AddCDForm";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <React.Fragment>
+        <AddCDForm />
+      </React.Fragment>
+    );
   }
 }
 
