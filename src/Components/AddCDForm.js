86a22e317a99885f65fdc79160599b980f1b74c2
bartosz <abraxaswisz@gmail.com>
2018-06-25 12:01:14 +0200

    changed edit cd form

--- src/Components/AddCDForm.js
+++ src/Components/AddCDForm.js
@@ -21,10 +21,10 @@ class AddCDForm extends Component {
       <Fragment>
         <form onSubmit={this.createCD}>
           <label htmlFor="artist">Artist</label>
-          <input ref={this.artistRef} id="artist" type="text" />
+          <input ref={this.artistRef} required id="artist" type="text" />
           <br />
           <label htmlFor="album">Album</label>
-          <input ref={this.albumRef} id="album" type="text" />
+          <input ref={this.albumRef} required id="album" type="text" />
           <br />
           <label htmlFor="year">Year</label>
           <input ref={this.yearRef} id="year" type="text" />
--- src/Components/CD.js
+++ src/Components/CD.js
@@ -1,11 +1,11 @@
 import React, { Component, Fragment } from "react";
-import EditCDForm from "./EditCDForm";
 
 class CD extends Component {
   state = {
     editable: false
   };
-  makeEditable = cd => {
+
+  makeEditable = () => {
     this.setState({
       editable: !this.state.editable
     });
@@ -13,16 +13,44 @@ class CD extends Component {
   render() {
     const { artist, album, year } = this.props.cd;
     if (!this.props.cd) return null;
-    if (this.state.editable) return <EditCDForm />;
     return (
       <Fragment>
-        <h3>{artist}</h3>
-        <h4>{album}</h4>
-        <h5>{year}</h5>
-        <button onClick={() => this.props.removeCD(this.props.index)}>
-          remove CD
-        </button>
-        <button onClick={this.makeEditable}>Edit CD</button>
+        {this.state.editable ? (
+          <form>
+            <input
+              ref={this.artistRef}
+              id="artist"
+              type="text"
+              placeholder={artist}
+            />
+            <br />
+            <input
+              ref={this.albumRef}
+              id="album"
+              type="text"
+              placeholder={album}
+            />
+            <br />
+            <input
+              ref={this.yearRef}
+              id="year"
+              type="text"
+              placeholder={year}
+            />
+            <br />
+            <button type="submit">Save</button>
+          </form>
+        ) : (
+          <Fragment>
+            <h3>{artist}</h3>
+            <h4>{album}</h4>
+            <h5>{year}</h5>
+            <button onClick={() => this.props.removeCD(this.props.index)}>
+              remove CD
+            </button>
+            <button onClick={this.makeEditable}>Edit CD</button>
+          </Fragment>
+        )}
       </Fragment>
     );
   }
