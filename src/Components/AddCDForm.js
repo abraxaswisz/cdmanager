a6fdac53e305577f0a2991991030d45bdf8bd072
bartosz <abraxaswisz@gmail.com>
2018-06-27 00:56:18 +0200

    adding new cds works fine, but component needs cleaning

--- src/Components/AddCDForm.js
+++ src/Components/AddCDForm.js
@@ -4,11 +4,14 @@ import ArtistSearchResult from "./ArtistSearchResult";
 
 class AddCDForm extends Component {
   state = {
-    searchresults: []
+    searchArtistresult: [],
+    artistReleases: [],
+    searchAlbumresult: []
   };
   artistRef = React.createRef();
   albumRef = React.createRef();
   yearRef = React.createRef();
+  searchArtistRef = React.createRef();
   static propTypes = {
     addCD: PropTypes.func
   };
@@ -22,6 +25,11 @@ class AddCDForm extends Component {
     };
     this.props.addCD(cd);
     e.currentTarget.reset();
+    this.setState({
+      searchArtistresult: [],
+      artistReleases: [],
+      searchAlbumresult: []
+    });
   };
 
   getDiscogsArtist = query => {
@@ -38,19 +46,63 @@ class AddCDForm extends Component {
       .then(data => {
         if (query) {
           this.setState({
-            searchresults: [...data]
+            searchArtistresult: [...data]
           });
         } else {
           this.setState({
-            searchresults: []
+            searchArtistresult: []
           });
         }
       })
       .catch(error => console.log(error));
     return artistArray;
   };
-
+  pickArtist = e => {
+    const getArtistId = e.currentTarget.attributes.index.value;
+    const url = `https://api.discogs.com/artists/${getArtistId}`;
+    if (getArtistId) {
+      fetch(url)
+        .then(data => data.json())
+        .then(json => (this.artistRef.current.value = json.name))
+        .then(() => fetch(`${url}/releases?&per_page=100`))
+        .then(res => res.json())
+        .then(json => this.setState({ artistReleases: [...json.releases] }))
+        .catch(err => console.log(err));
+    }
+  };
+  filterAlbum = () => {
+    let albumRef = this.albumRef.current.value;
+    const { artistReleases } = this.state;
+    const filteredArray = [];
+    console.log(albumRef);
+    if (this.albumRef.current.value) {
+      artistReleases.filter(album => {
+        let albumToLower = album.title.toString().toLowerCase();
+        if (albumToLower.includes(albumRef.toLowerCase())) {
+          filteredArray.push(album);
+        }
+      });
+    }
+    this.setState({
+      searchAlbumresult: [...filteredArray]
+    });
+  };
+  pickAlbum = e => {
+    const { searchAlbumresult } = this.state;
+    const getAlbumId = e.currentTarget.attributes.index.value;
+    const getYear = searchAlbumresult.find(album => album.id == getAlbumId)
+      .year;
+    const getName = searchAlbumresult.find(album => album.id == getAlbumId)
+      .title;
+    this.yearRef.current.value = getYear;
+    this.albumRef.current.value = getName;
+  };
   render() {
+    const {
+      searchArtistresult,
+      artistReleases,
+      searchAlbumresult
+    } = this.state;
     return (
       <Fragment>
         <form onSubmit={this.createCD}>
@@ -63,15 +115,44 @@ class AddCDForm extends Component {
             type="text"
           />
           <ul>
-            {this.state.searchresults
-              ? this.state.searchresults.map(result => {
-                  return <li key={result.id}>{result.title}</li>;
+            {searchArtistresult
+              ? searchArtistresult.map(result => {
+                  return (
+                    <li
+                      onClick={this.pickArtist}
+                      ref={this.searchArtistRef}
+                      key={result.id}
+                      index={result.id}
+                    >
+                      {result.title}
+                    </li>
+                  );
                 })
               : null}
           </ul>
           <br />
           <label htmlFor="album">Album</label>
-          <input ref={this.albumRef} required id="album" type="text" />
+          <input
+            ref={this.albumRef}
+            required
+            id="album"
+            type="text"
+            onChange={this.filterAlbum}
+          />
+          <ul>
+            {searchAlbumresult.map(album => {
+              return (
+                <li
+                  onClick={this.pickAlbum}
+                  ref={this.searchAlbumRef}
+                  index={album.id}
+                  key={album.id + 1}
+                >
+                  {album.title}
+                </li>
+              );
+            })}
+          </ul>
           <br />
           <label htmlFor="year">Year</label>
           <input ref={this.yearRef} id="year" type="number" step="1" />
